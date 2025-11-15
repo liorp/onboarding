e16d040cc14a5c05d4824f1368c3bfd3b4bdca6c
Lior Pollak <4294489+liorp@users.noreply.github.com>
2025-11-15 10:00:19 +0200

    feat: chrome

--- src/manual_apps.sh
+++ src/manual_apps.sh
@@ -2,7 +2,7 @@
 
 open_manual_apps() {
     echo "Opening Elasticvue (Chrome Extension)..."
-    open "https://chrome.google.com/webstore/detail/elasticvue/hkedbapjpblbodpgbajblpnlpenaebaa"
+    open -a "Google Chrome" "https://chrome.google.com/webstore/detail/elasticvue/hkedbapjpblbodpgbajblpnlpenaebaa"
 
     echo "Opening Time Out..."
     open "https://www.dejal.com/timeout/"

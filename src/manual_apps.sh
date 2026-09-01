abbba85dae9b4bcf5a5cce77e286e51bf467b91d
Lior Polak <4294489+liorp@users.noreply.github.com>
2026-09-01 12:28:33 +0300

    feat: install Time Out with Homebrew

--- src/manual_apps.sh
+++ src/manual_apps.sh
@@ -3,9 +3,6 @@
 open_manual_apps() {
     echo "Opening Elasticvue (Chrome Extension)..."
     open -a "Google Chrome" "https://chrome.google.com/webstore/detail/elasticvue/hkedbapjpblbodpgbajblpnlpenaebaa"
-
-    echo "Opening Time Out..."
-    open "https://www.dejal.com/timeout/"
 }
 
 open_manual_apps
